b8ae00850ccf7a47088e3531aaa7ca92d470c3de
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-19 16:09:51 -0700

    setting up a PORT to help with R10 error/testing solution

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ const PlayerShip = require('./player_ship');
 const Entities = require('./entities');
 const Game = require('./game');
 const Board = require('./board');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 document.addEventListener("DOMContentLoaded", () => {
     const gameBoard = document.getElementById('game-canvas');
