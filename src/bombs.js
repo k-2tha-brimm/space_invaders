c6c669b8d1e85f6cfa63e98fdf7855184beb7d97
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-19 15:52:50 -0700

    add application to Heroku

--- src/bombs.js
+++ src/bombs.js
@@ -4,7 +4,7 @@ class Bomb extends Entities {
     constructor(position) {
         super(position);
         this.pos = position[0];
-        this.vel = [0, 1];
+        this.vel = [0, 1.6];
         this.height = 5;
         this.width = 2.5;
         this.color = 'white';
