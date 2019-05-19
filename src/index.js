8bce5b30e4e5dc3227a8c0f23f8f7e0c9bf40b6b
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-19 16:16:25 -0700

    remove unnecessary configuration

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 document.addEventListener("DOMContentLoaded", () => {
