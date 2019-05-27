f2667c877d0044b2c836020fe7d2981d5086d303
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-27 02:03:23 -0700

    add simple express backend for heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5000);
