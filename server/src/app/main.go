fe4f9bb3edbf8ce8ca752607549c45360b9183a2
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-27 00:47:09 -0700

    add start script for server

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello, Kevin!")
 	}))
 
-	log.Println("Now server is running on Port 3000")
-	http.ListenAndServe(":3000", nil)
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	log.Printf("Now server is running on Port %v\n", port)
+	http.ListenAndServe(":" + port, nil)
 }
